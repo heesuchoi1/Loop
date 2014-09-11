24da84f9c151b7660b22811d094584bb47106d3e
heesuchoi1 <heesu7563@gmail.com>
2014-09-11 15:55:53 +0900

    dsssssss

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 function setup() {
   createCanvas(640, 480);
-  background(128, 0, 64);
+
   noFill();
 
   stroke(255, 0, 0);
@@ -18,8 +18,15 @@ function setup() {
   ellipse(x * 150, 100, 50, 50);
   */
 
+}
+
+function draw(){
+  background(128, 0, 64);
   for (var x = 1; x <=8; x++)
   {
     ellipse(x * 70, x * 20 + 100, x + 50 , x * 10);
   }
+
+  var size = Math.abs(mouseX - 320);
+  ellipse(320, 240,size,size);
 }
